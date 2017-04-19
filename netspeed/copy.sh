9ee3c57a75e725b93e258a379b16daf04d950fb1
Mystery0 <mystery0dyl520@gmail.com>
2017-04-19 06:26:54 +0000

    new sh

---
+++ netspeed/copy.sh
@@ -0,0 +1,3 @@
+cp Mystery0Traffic.java ~/LineageOS/frameworks/base/packages/SystemUI/src/com/android/systemui/statusbar/policy/
+cp mystery0_traffic.xml ~/LineageOS/frameworks/base/packages/SystemUI/res/layout/
+vim ~/LineageOS/frameworks/base/packages/SystemUI/res/layout/status_bar.xml
