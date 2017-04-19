9e1bc41db7fecc7f9587cc2970088bf0cc7364c3
Mystery0 <mystery0dyl520@gmail.com>
2017-04-19 03:25:31 +0000

    correct

--- change.sh
+++ change.sh
@@ -2,5 +2,4 @@ cp fonts/NotoSansCJK-Regular.ttc ~/LineageOS/external/noto-fonts/cjk/NotoSansCJK
 cp fonts/Roboto-Regular.ttf ~/LineageOS/external/roboto-fonts/Roboto-Regular.ttf
 cp fonts/RobotoCondensed-Regular.ttf ~/LineageOS/external/roboto-fonts/RobotoCondensed-Regular.ttf
 cp fonts/DroidSansMono.ttf ~/LineageOS/frameworks/base/data/fonts/DroidSansMono.ttf
-
 cp -rf SystemUI ~/LineageOS/frameworks/base/packages/SystemUI/res
