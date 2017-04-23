65ac75e162c9e28b7423bad593866c9625ffacc0
Mystery0 <mystery0dyl520@gmail.com>
2017-04-23 18:29:59 +0800

    correct

--- ready.sh
+++ ready.sh
@@ -1,7 +1,7 @@
 sudo apt-get update && sudo apt-get install git-core gnupg flex bc bison gperf libsdl1.2-dev libesd0-dev libwxgtk3.0-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-8-jre openjdk-8-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev gcc-multilib maven tmux screen w3m ncftp liblz4-tool python nginx unzip -y
 wget https://www.imagemagick.org/download/ImageMagick.tar.gz
 tar -zxvf ImageMagick.tar.gz
-cd ImageMagick*
+cd ImageMagick-7*
 ./configure
 make
 make install
