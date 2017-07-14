b7d440b4eec7413a2454819bd9dab75f28a8a828
Mystery0 <mystery0dyl520@gmail.com>
2017-07-14 12:13:13 +0000

    correct

--- edit.sh
+++ edit.sh
@@ -4,5 +4,5 @@ vim ~/LineageOS/frameworks/base/core/res/res/values/themes_material.xml
 vim ~/LineageOS/frameworks/base/core/res/res/values/config.xml
 vim ~/LineageOS/frameworks/base/packages/SystemUI/res/layout/status_bar.xml
 vim ~/LineageOS/frameworks/base/packages/SystemUI/res/values/dimens.xml
-vim ~/LineageOS/frameworks/base/packages/SystemUI/res/values/id.xml
+vim ~/LineageOS/frameworks/base/packages/SystemUI/res/values/ids.xml
 vim ~/LineageOS/frameworks/base/packages/SystemUI/res/values/styles.xml
