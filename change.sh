d313808adb58c85d98266af557fe223b1f8a6987
Mystery00 <mystery0dyl520@gmail.com>
2017-07-14 21:11:19 +0800

    correct

--- change.sh
+++ change.sh
@@ -3,7 +3,7 @@ cp fonts/Roboto-Regular.ttf ~/LineageOS/external/roboto-fonts/Roboto-Regular.ttf
 cp fonts/RobotoCondensed-Regular.ttf ~/LineageOS/external/roboto-fonts/RobotoCondensed-Regular.ttf
 cp fonts/DroidSansMono.ttf ~/LineageOS/frameworks/base/data/fonts/DroidSansMono.ttf
 cp netspeed/Mystery0Traffic.java ~/LineageOS/frameworks/base/packages/SystemUI/src/com/android/systemui/statusbar/policy/
-cp SystemUI/drawable/stat_sys_vpn_ic.xml ~/LineageOS/frameworks/base/packages/SystemUI/res/drawable/
+cp SystemUI/drawable/* ~/LineageOS/frameworks/base/packages/SystemUI/res/drawable/
 cp SystemUI/drawable-xxxhdpi/* ~/LineageOS/frameworks/base/packages/SystemUI/res/drawable-xxxhdpi/
 mkdir ~/LineageOS/frameworks/base/packages/SystemUI/src/com/google
 mkdir ~/LineageOS/frameworks/base/packages/SystemUI/src/com/google/android
